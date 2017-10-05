62567bfe37224156171ed62d40aecd57a09a977f
wyattlarson <wyattlarson250@gmail.com>
2017-10-05 17:38:33 -0600

    10/5/2014 5:38pm. Finished Location and associated it with Character class.

--- flood/src/byui/cit260/flood/model/Location.java
+++ flood/src/byui/cit260/flood/model/Location.java
@@ -12,8 +12,90 @@ import java.io.Serializable;
  * @author wyatt
  */
 public class Location implements Serializable{
+    
+//------------------------------Location Attributes------------------------------//
     private int locationNumber;
     private boolean visited;
     private int amountRemaining;
+    
+//------------------------------Associations------------------------------//
+    //this is the association between the Location class and the Character class
+    private Character character;
+                public Character getCharacter() {
+                    return character;
+                }
+                public void setCharacter(Character character) {
+                    this.character = character;
+                }
+          
+      //this is the association between Location and BuildingScene
+                
+    //Constructor
+    public Location() {
+    }
+
+//------------------------------Public Functions------------------------------//
+    public int getLocationNumber() {
+        return locationNumber;
+    }
+
+    public void setLocationNumber(int locationNumber) {
+        this.locationNumber = locationNumber;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public int getAmountRemaining() {
+        return amountRemaining;
+    }
+
+    public void setAmountRemaining(int amountRemaining) {
+        this.amountRemaining = amountRemaining;
+    }
+    
+//------------------------------Overrides------------------------------//
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + this.locationNumber;
+        hash = 19 * hash + (this.visited ? 1 : 0);
+        hash = 19 * hash + this.amountRemaining;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "locationNumber=" + locationNumber + ", visited=" + visited + ", amountRemaining=" + amountRemaining + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (this.locationNumber != other.locationNumber) {
+            return false;
+        }
+        if (this.visited != other.visited) {
+            return false;
+        }
+        if (this.amountRemaining != other.amountRemaining) {
+            return false;
+        }
+        return true;
+    }
     
 }
