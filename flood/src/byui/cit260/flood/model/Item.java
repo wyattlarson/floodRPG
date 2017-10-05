2927e7faac8cc5fa70907d81ffccc43bd687c0d5
Brandon <Brandon@10.38.164.60>
2017-10-05 12:22:46 -0700

    Item class

---
+++ flood/src/byui/cit260/flood/model/Item.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.flood.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author Brandon
+ */
+public class Item implements Serializable{
+    
+    private String description;
+    private int itemId;
+
+    public Item() {
+    }
+    
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 31 * hash + Objects.hashCode(this.description);
+        hash = 31 * hash + this.itemId;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "description=" + description + ", itemId=" + itemId + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Item other = (Item) obj;
+        if (this.itemId != other.itemId) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
