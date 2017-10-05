a7ea6776c28e5331d7d757c3663b82865afca4c3
wyattlarson <wyattlarson250@gmail.com>
2017-10-05 13:16:16 -0600

    In class model classes

---
+++ flood/src/byui/cit260/flood/model/Game.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.flood.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author wyatt
+ */
+public class Game implements Serializable{
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + (int) (Double.doubleToLongBits(this.amountSaved) ^ (Double.doubleToLongBits(this.amountSaved) >>> 32));
+        hash = 23 * hash + Objects.hashCode(this.savedFile);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "amountSaved=" + amountSaved + ", savedFile=" + savedFile + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.amountSaved) != Double.doubleToLongBits(other.amountSaved)) {
+            return false;
+        }
+        if (!Objects.equals(this.savedFile, other.savedFile)) {
+            return false;
+        }
+        return true;
+    }
+
+    private double amountSaved;
+    private String savedFile;
+    public Game() {
+    }
+
+    public double getAmountSaved() {
+        return amountSaved;
+    }
+
+    public void setAmountSaved(double amountSaved) {
+        this.amountSaved = amountSaved;
+    }
+
+    public String getSavedFile() {
+        return savedFile;
+    }
+
+    public void setSavedFile(String savedFile) {
+        this.savedFile = savedFile;
+    }
+    
+}
---
+++ flood/src/byui/cit260/flood/model/Player.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.flood.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author wyatt
+ */
+public class Player implements Serializable{
+    private String name;
+    private String lastDatePlayed;
+    private double bestScore;
+
+    public Player() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return lastDatePlayed;
+    }
+
+    public void setDate(String date) {
+        this.lastDatePlayed = date;
+    }
+
+    public double getBestScore() {
+        return bestScore;
+    }
+
+    public void setBestScore(double bestScore) {
+        this.bestScore = bestScore;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + Objects.hashCode(this.name);
+        hash = 11 * hash + Objects.hashCode(this.lastDatePlayed);
+        hash = 11 * hash + (int) (Double.doubleToLongBits(this.bestScore) ^ (Double.doubleToLongBits(this.bestScore) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", date=" + lastDatePlayed + ", bestScore=" + bestScore + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.bestScore) != Double.doubleToLongBits(other.bestScore)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastDatePlayed, other.lastDatePlayed)) {
+            return false;
+        }
+        return true;
+    }
+   
+    
+}
