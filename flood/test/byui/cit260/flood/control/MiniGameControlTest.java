4ab42fd0dc177b1741924db442bab1f79b247705
RC03 <rncall92@gmail.com>
2017-10-20 13:25:20 -0600

    Merge origin/master
    Conflicts:
    	flood/src/byui/cit260/flood/model/QuestionScene.java

--- flood/test/byui/cit260/flood/control/MiniGameControlTest.java
+++ flood/test/byui/cit260/flood/control/MiniGameControlTest.java
@@ -29,9 +29,7 @@ public class MiniGameControlTest {
         double guess = 0.0;
         String expResult = "";
         String result = MiniGameControl.miniGame1(base1, base2, height, guess);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+     
     }
 
     /**
@@ -47,6 +45,56 @@ public class MiniGameControlTest {
     
    
     }
+    @Test
+        public void testMiniGame2a() {
+        System.out.println("miniGame2");
+        double length = -1.0;
+        double width = 20.0;
+        double expResult = -1;
+        double result = MiniGameControl.miniGame2(length, width);
+    
+   
+    }
+        @Test
+        public void testMiniGame2b() {
+        System.out.println("miniGame2");
+        double length = 10.0;
+        double width = -1.0;
+        double expResult = -1.0;
+        double result = MiniGameControl.miniGame2(length, width);
+    
+   
+    }
+        @Test
+        public void testMiniGame2c() {
+        System.out.println("miniGame2");
+        double length = 1.0;
+        double width = 30.0;
+        double expResult = 62.0;
+        double result = MiniGameControl.miniGame2(length, width);
+    
+   
+    }
+        @Test
+        public void testMiniGame2d() {
+        System.out.println("miniGame2");
+        double length = 30.0;
+        double width = 1.0;
+        double expResult = 62.0;
+        double result = MiniGameControl.miniGame2(length, width);
+    
+   
+    }
+        @Test
+        public void testMiniGame2e() {
+        System.out.println("miniGame2");
+        double length = 30.0;
+        double width = 30.0;
+        double expResult = 120.0;
+        double result = MiniGameControl.miniGame2(length, width);
+    
+   
+    }
 
     /**
      * Test of miniGame3 method, of class MiniGameControl.
@@ -54,12 +102,59 @@ public class MiniGameControlTest {
     @Test
     public void testMiniGame3() {
         System.out.println("miniGame3");
-        double length = 0.0;
-        double height = 0.0;
-        double expResult = 0.0;
+        double length = 10.0;
+        double height = 15.0;
+        double expResult = 416.0;
         double result = MiniGameControl.miniGame3(length, height);
        
  
     }
-    
+        @Test
+    public void testMiniGame3a() {
+        System.out.println("miniGame3");
+        double length = -1.0;
+        double height = 15.0;
+        double expResult = -1.0;
+        double result = MiniGameControl.miniGame3(length, height);
+       
+ 
+    }
+        @Test
+    public void testMiniGame3b() {
+        System.out.println("miniGame3");
+        double length = 10.0;
+        double height = -1.0;
+        double expResult = -1.0;
+        double result = MiniGameControl.miniGame3(length, height);
+    }   
+        @Test
+    public void testMiniGame3c() {
+        System.out.println("miniGame3");
+        double length = 1.0;
+        double height = 30.0;
+        double expResult = 61.0;
+        double result = MiniGameControl.miniGame3(length, height);
+       
+ 
+    }
+        @Test
+    public void testMiniGame3d() {
+        System.out.println("miniGame3");
+        double length = 30.0;
+        double height = 1.0;
+        double expResult = 1802.0;
+        double result = MiniGameControl.miniGame3(length, height);
+       
+ 
+    }
+         @Test
+    public void testMiniGame3e() {
+        System.out.println("miniGame3");
+        double length = 30.0;
+        double height = 30.0;
+        double expResult = 2912.0;
+        double result = MiniGameControl.miniGame3(length, height);
+       
+ 
+    }
 }
