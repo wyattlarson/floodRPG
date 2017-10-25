4e10cb9a5ee02de9ab3a2e8af640b9e8f015898d
RC03 <rncall92@gmail.com>
2017-10-25 13:44:54 -0600

    Merge origin/master
    Conflicts:
    	flood/src/byui/cit260/flood/model/QuestionScene.java

--- flood/src/byui/cit260/flood/viewLayer/StartProgramView.java
+++ flood/src/byui/cit260/flood/viewLayer/StartProgramView.java
@@ -10,15 +10,31 @@ package byui.cit260.flood.viewLayer;
  * @author Ryan Call
  */
 public class StartProgramView {
-   public void displayStartProgramView() {
+   public boolean display() {
        boolean endView=false;
        do{ 
            String inputs[] = getInputs();
-           if (inputs = void || inputs[0] = "Q") {
+           if (inputs.length < 1 || inputs[0].toUpperCase().equals("Q")) {
                return endView = doAction(inputs);
-               
            } 
        }
-       while endView != true;
+       while (endView != true);
+       return false;
+   
    }
+
+    private String[] getInputs() {
+        System.out.println("****called getInputs()****");
+        String[] inputs = new String[1];
+        inputs[0] = "testInput";
+        return inputs;
+    }
+
+    private boolean doAction(String[] inputs) {
+                System.out.println("****called doAction****");
+                System.out.println("\tinputs = " + inputs[0]);
+                
+                return true;
+
+    }
 }
