567e4df828c5e29e25478775c03d226fb1f3ea27
wyattlarson <wyattlarson250@gmail.com>
2017-12-11 13:39:36 -0700

    MiniGame FIx

--- flood/src/byui/cit260/flood/viewLayer/Minigame3View.java
+++ flood/src/byui/cit260/flood/viewLayer/Minigame3View.java
@@ -10,6 +10,7 @@ import static jdk.nashorn.internal.objects.NativeString.trim;
 import byui.cit260.flood.control.GameControl;
 import byui.cit260.flood.model.Player;
 import byui.cit260.flood.control.MiniGameControl;
+import byui.cit260.flood.exceptions.GameControlException;
 import byui.cit260.flood.exceptions.MiniGameControlException;
 import byui.cit260.flood.model.Game;
 import byui.cit260.flood.model.Item;
@@ -23,7 +24,7 @@ import java.util.logging.Logger;
  * @author Ryan
  */
 public class Minigame3View extends View {
-    
+
     public String[] getInputs() {
         String[] inputs = new String[2];
         this.console.println("Using the Quadratic equation,"
@@ -35,38 +36,28 @@ public class Minigame3View extends View {
 
     public boolean doAction(String[] inputs) {
 
-                        String xGuess = inputs[0];
-                        try{
-                        double xDoub = Double.parseDouble(xGuess);
-                        try {
-                            double answer = MiniGameControl.miniGame3(xDoub);
-                            this.console.println("You are correct! You have received a Golden Key");
-                            pickUpKey();
-                        } catch (MiniGameControlException e) {
-                            this.console.println(e.getMessage());
-                            return false;
-                        }
-                        }
-                        catch(NumberFormatException e){
-                            ErrorView.display(this.getClass().getName(),"Invalid inputs, use number.");
-                        }
-                       return true;
-                    } 
+        String xGuess = inputs[0];
+        try {
+            double xDoub = Double.parseDouble(xGuess);
+            try {
+                double answer = MiniGameControl.miniGame3(xDoub);
+                this.console.println("You are correct! You have received a Golden Key");
+                pickUpKey();
+            } catch (MiniGameControlException e) {
+                this.console.println(e.getMessage());
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            ErrorView.display(this.getClass().getName(), "Invalid inputs, use number.");
+        }
+        return true;
+    }
 
     private void pickUpKey() {
-        Game game = Flood.getCurrentGame();
-        Item[] items = game.getItems();
-        ArrayList<Item> inventory = new ArrayList<>();
-        for (Item item : items) {
-            if (item.getName() == "GoldKey") {
-                //inventory.add(item.getName());
-                item.setInInventory(true);
-                this.console.println("Golden key was added to your inventory.\n");
-            }
+        try {
+            GameControl.addItemToInventory("GoldKey");
+        } catch (GameControlException ex) {
+            ErrorView.display("MiniGame3", ex.getMessage());
         }
-        game.setInventory(inventory);
     }
-    }
-
-  
-        
+}
